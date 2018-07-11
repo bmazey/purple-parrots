b17c0cb3df242cce16d4935f3445de01301bb881
leohsu2906 <lh2937@gmail.com>
2018-07-11 17:41:14 -0400

    with dependency

--- src/main/java/org/columbia/config/WebSocketConfig.java
+++ src/main/java/org/columbia/config/WebSocketConfig.java
@@ -1,5 +1,5 @@
 package org.columbia.config;
-//hhhh
+
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,11 +16,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws").withSockJS();
     }
 
-    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+
+        // Use this for enabling a Full featured broker like RabbitMQ
+//        registry.enableStompBrokerRelay("/topic")
+//                .setRelayHost("localhost")
+//                .setRelayPort(61613)
+//                .setClientLogin("guest")
+//                .setClientPasscode("guest");
     }
+
+
 }
+
 
 
